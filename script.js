f01438f1885ba9d378f6232e82665e04bb50cc6f
ChustrupFx <42353254+ChustrupFx@users.noreply.github.com>
2020-12-10 22:55:30 -0300

    Adicionado funcionalidade de pausar e parar

--- script.js
+++ script.js
@@ -1,30 +1,65 @@
 const cronometer = document.querySelector('div')
-const button = document.querySelector('button')
+const startCronometerBtn = document.querySelector('.startCronometer')
+const pauseCronometerBtn = document.querySelector('.pauseCronometer')
+const stopCronometerBtn = document.querySelector('.stopCronometer')
 
-button.addEventListener('click', e => {
+startCronometerBtn.addEventListener('click', e => {
 
     turnOnCronometer()
+    pauseCronometerBtn.classList.remove('hide')
+    stopCronometerBtn.classList.remove('hide')
+    startCronometerBtn.classList.add('hide')
 
 })
 
-turnOnCronometer()
+pauseCronometerBtn.addEventListener('click', e => {
+
+    pauseCronometer()
+    pauseCronometerBtn.classList.add('hide')
+    startCronometerBtn.classList.remove('hide')
+
+})
+
+stopCronometerBtn.addEventListener('click', e => {
+
+    stopCronometer()
+    pauseCronometerBtn.classList.add('hide')
+    stopCronometerBtn.classList.add('hide')
+    startCronometerBtn.classList.remove('hide')
+
+})
+
+const cronometerData = {
+    elapsedTimeInSeconds: 0,
+    cronometerLoop: null
+}
 
 function turnOnCronometer() {
+    
+    const cronometerLoop = setInterval(() => {
+        cronometerData.elapsedTimeInSeconds += 1
 
-    const initTime = Date.now()
-
-    setInterval(() => {
-        const newTime = Date.now()
-
-        const elapsedTimeInMiliseconds = newTime - initTime
-
-        const elapsedSeconds = Math.floor(elapsedTimeInMiliseconds / 1000) % 60
-        const elapsedMinutes = Math.floor(elapsedTimeInMiliseconds / 1000 / 60) % 60
-        const elapsedHours = Math.floor(elapsedTimeInMiliseconds / 1000 / 60 / 60) 
-
-        cronometer.innerHTML = `${formattedNumber(elapsedHours)}:${formattedNumber(elapsedMinutes)}:${formattedNumber(elapsedSeconds)}`
+        const elapsedSeconds = Math.floor(cronometerData.elapsedTimeInSeconds) % 60
+        const elapsedMinutes = Math.floor(cronometerData.elapsedTimeInSeconds / 60) % 60
+        const elapsedHours = Math.floor(cronometerData.elapsedTimeInSeconds / 60 / 60) 
+        
+        cronometer.innerHTML = formattedNumber(elapsedHours) + ':' + 
+                                formattedNumber(elapsedMinutes) + ':' +
+                                formattedNumber(elapsedSeconds)
+        
+        cronometerData.cronometerLoop = cronometerLoop
     }, 1000)
+    cronometerData.cronometerLoop = cronometerLoop
+}
 
+function pauseCronometer() {
+    clearInterval(cronometerData.cronometerLoop)
+}
+
+function stopCronometer() {
+    clearInterval(cronometerData.cronometerLoop)
+    cronometerData.elapsedTimeInSeconds = 0
+    cronometer.innerHTML = '00:00:00'
 }
 
 function formattedNumber(number) {
